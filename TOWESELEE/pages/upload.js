8c46b24c50584bbffadce5a2b47b73cb617b253c
Wojtkowskyy <pasztetw@gmail.com>
2025-05-06 23:21:50 +0200

    Updatde upload.js

--- TOWESELEE/pages/upload.js
+++ TOWESELEE/pages/upload.js
@@ -40,6 +40,7 @@ export default function Upload() {
   return (
     <div className="min-h-screen bg-beige flex flex-col items-center justify-center text-center p-6 animate-fade-in">
       <h1 className="text-2xl font-bold mb-2 animate-slide-in">Mamy nadzieję, ze Twoja galeria jest pełna zdjęć i filmów!</h1>
+      <h2 className="text-lg font-light italic text-gray-600">Pamiętaj, aby przesłać również później coś nowego!</h2>
 
       <form onSubmit={handleSubmit} className="flex flex-col items-center space-y-4 w-full max-w-md">
         <input
